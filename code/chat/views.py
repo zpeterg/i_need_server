028fdb195cd37fb27fa588ca20b8701bb06d75c3
Peter Granderson <solace@fastmail.fm>
2019-02-26 10:18:25 -0600

    add: take post requests

--- code/chat/views.py
+++ code/chat/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from respond.main import Respond
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
 
+@csrf_exempt
 def index(request):
-    print('REQUEST:::', request)
+    print('REQUEST:::', request.POST.get('user_id'), request.POST.get('phrase'))
+    phrase = request.POST.get('user_id')
+    phrase = request.POST.get('phrase')
     subject = ()
     respond = Respond()
-    send_phrase = respond.get_response('food', subject)
+    send_phrase = respond.get_response(phrase, subject)
     return HttpResponse(f"Response to 'food' is: {send_phrase}")
