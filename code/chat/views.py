675dae1a433e1bbae20b8bd7688aba427de23a64
Peter Granderson <solace@fastmail.fm>
2019-02-26 12:37:14 -0600

    work on adding db

--- code/chat/views.py
+++ code/chat/views.py
@@ -1,17 +1,32 @@
 from django.http import HttpResponse
 from respond.main import Respond
 from django.views.decorators.csrf import csrf_exempt
-
+from pymongo import MongoClient
+client = MongoClient('198.168.99.100', 27017)
+db = client.sessions
+sessions = client.sessions
 
 # Create your views here.
 
 
 @csrf_exempt
 def index(request):
-    print('REQUEST:::', request.POST.get('user_id'), request.POST.get('phrase'))
-    phrase = request.POST.get('user_id')
-    phrase = request.POST.get('phrase')
     subject = ()
+    user_id = request.POST.get('user_id')
+    phrase = request.POST.get('phrase')
+    session = sessions.find_one({'user_id': user_id})
+    print('REQUEST:::', user_id, phrase, 'SESSION:::', session)
+    # If session exists for user, retrieve and delete it
+    if session:
+        subject = session['subject']
+        sessions.delete_many({'user_id': user_id})
     respond = Respond()
     send_phrase = respond.get_response(phrase, subject)
+    # Create session for user
+    if send_phrase['subject']:
+        reply = sessions.insert_one({
+            'user_id': user_id,
+            'subject': subject,
+        })
+        print('DB REPLY:::', reply)
     return HttpResponse(f"Response to 'food' is: {send_phrase}")
